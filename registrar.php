b905a47571b3732f4919bc89865146abbbe5ecda
Molivareztl <marcos.olivares.t1vl@gmail.com>
2025-11-02 14:41:34 -0300

    no ahi si que no se

--- registrar.php
+++ registrar.php
@@ -1,5 +1,7 @@
 <?php
-
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 // Datos de conexión a la base de datos
 $host = "localhost";
 $db   = "samazon";
@@ -33,7 +35,6 @@ if ($password !== $confirm_password) {
 $stmt = $conn->prepare("INSERT INTO usuario (Nombre, Apellido, EMail, Contraseña, Numero_telefono, DNI) VALUES (?, ?, ?, ?, ?, ?)");
 $hashed_password = password_hash($password, PASSWORD_DEFAULT); // Hashear contraseña
 $stmt->bind_param("ssssii", $nombre, $apellido, $correo, $hashed_password, $telefono, $dni);
-
 if ($stmt->execute()) {
     // Redirigir a Guterake.php
     header("Location: Guterake.php");
@@ -44,4 +45,3 @@ if ($stmt->execute()) {
 
 $stmt->close();
 $conn->close();
-?>
